0a7f58bda06b054a1b74db87b8d8fe5ad3b761d0
alvarollmenezes <alvarollmenezes@gmail.com>
2016-08-17 16:30:44 -0300

    Fix variáveis de configuração

--- src/config/dio.js
+++ src/config/dio.js
@@ -1,6 +1,6 @@
 const dioLatestUrl = process.env.DIO_LATEST_URL || 'https://dio.es.gov.br/apifront/portal/edicoes/ultimas_edicoes/';
 const dioSearchUrl = process.env.DIO_BUSCA_URL || 'http://www.dio.es.gov.br/busca/busca/buscar';
-const pdfUrl = process.env.DIO_BUSCA_URL || 'http://cdn.dio.es.gov.br/portal/edicoes/download';
+const pdfUrl = process.env.DIO_PDF_URL || 'http://dio.es.gov.br/portal/edicoes/download';
 const sortParam = process.env.SORT_PARAM || 'sort=';
 const sortDefault = process.env.SORT_DEFAULT || 'date';
 
