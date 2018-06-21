fc2654058698d74b5214a0fc1ec7d0ee1cfbd8a2
alvarollmenezes <alvarollmenezes@gmail.com>
2018-06-21 17:19:10 -0300

    update dio hosts
    [skip ci]

--- src/config/dio.js
+++ src/config/dio.js
@@ -1,6 +1,6 @@
-const dioLatestUrl = process.env.DIO_LATEST_URL || 'http://dio.es.gov.br/apifront/portal/edicoes/ultimas_edicoes/';
-const dioSearchUrl = process.env.DIO_BUSCA_URL || 'http://www.dio.es.gov.br/busca/busca/buscar';
-const pdfUrl = process.env.DIO_PDF_URL || 'http://dio.es.gov.br/portal/edicoes/download';
+const dioLatestUrl = process.env.DIO_LATEST_URL || 'http://ioes.dio.es.gov.br/apifront/portal/edicoes/ultimas_edicoes/';
+const dioSearchUrl = process.env.DIO_BUSCA_URL || 'http://ioes.dio.es.gov.br/busca/busca/buscar';
+const pdfUrl = process.env.DIO_PDF_URL || 'http://ioes.dio.es.gov.br/portal/edicoes/download';
 const sortParam = process.env.SORT_PARAM || 'sort=';
 const sortDefault = process.env.SORT_DEFAULT || 'date';
 
